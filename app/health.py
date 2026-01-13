c0f43d8709f462daf158a1158916204b59c41c4b
NotManeesh23 <maneeshprog@gmail.com>
2026-01-14 01:26:50 +0530

    Update health.py

--- app/health.py
+++ app/health.py
@@ -6,3 +6,7 @@ router=APIRouter(prefix="/health", tags=["Health"])
 @router.get("/")
 def health_check():
     return{"status":"OK"}
+
+@router.get("/First_test")
+def test():
+    return{"Message":"Omaala naanga dha"}
